200a1da156217ade2aa12699b76d6d6bb4c0fb62
Alexander Kozarev <al1102@yandex.ru>
2017-04-02 00:46:57 +0600

    added counter of weeks

---
+++ count_of_week.py
@@ -0,0 +1,21 @@
+# this script can show, how much weeks you need to run X km.
+# base_km - what you can run
+# wish_km - what you want to run
+
+
+print("How much km you can run?")		
+base_km = int(input())		
+print("How much km you want to run?")	
+wish_km = int(input())
+
+def count_of_week(base_km, wish_km):
+	week = 0
+	while base_km < wish_km:
+		base_km = base_km * 1.1
+		week += 1
+		print("week ", week, "; km ", round(base_km, 2))
+
+	
+
+count_of_week(base_km, wish_km)
+input()
