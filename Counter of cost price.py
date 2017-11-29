fca4a87810b258e2766038262201438a899b039b
Alexander Kozarev <al1102@yandex.ru>
2017-11-29 14:06:41 +0500

    counter of cost

--- Counter of cost price.py
+++ Counter of cost price.py
@@ -1,16 +1,18 @@
-﻿''' Counter of cost price '''
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-invoice = float(input("Your invoice price: "))
-eur_rub = float(input("eur/rub: "))
-customs = float(input("Customs price: "))
+
+invoice = float(input("Итоговая цена Инвойса (в eur): "))
+eur_rub = float(input("Курс eur/rub: "))
+customs = float(input("Цена таможни: "))
 
 total_price = invoice * eur_rub + customs
 
-count_of_goods = int(input("How much goods bought: "))
+count_of_goods = int(input("Количество товаров: "))
 prices = []
 summ_price_of_goods = 0
 for i in range(count_of_goods):
-	price = float(input("Cost of commodity: "))
+	price = float(input("Цена позиции (в eur): "))
 	summ_price_of_goods += price
 	prices.append(price)
 	
